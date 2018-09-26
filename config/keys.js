53300148169c5f7ef7d315773353f9699da4ee55
lokibeat <everardtx@gmail.com>
2018-09-26 15:10:12 -0500

    express set up complete

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI:
+    'mongodb://genietest:genietest1@ds115753.mlab.com:15753/genieshopper'
+}
--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
 
 const app = express();
 
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+//connect to mongodb
+
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+
 app.get('/', (req, res) => res.send('Hello World!'));
+
+//Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
 
 const port = process.env.PORT || 5000 ;
 
