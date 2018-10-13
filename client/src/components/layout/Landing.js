2ba1fc8f2da5125e4257531854e3624a4fd9f829
lokibeat <37983312+lokibeat@users.noreply.github.com>
2018-10-13 10:17:34 -0500

    logic to differentiate authenticated user

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -13,22 +13,38 @@ class Landing extends Component {
   }
   
   render() {
-    return (
-        <div className="landing">
-        <div className="dark-overlay landing-inner text-light">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Genie Shopper
-                </h1>
-                <p className="lead"> Shop electricity programs and providers with your customized usage profile.</p>
-                <hr />
-                <Link to="/register" className="btn btn-lg btn-info mr-2">Sign Up</Link>
-                <Link to="/login" className="btn btn-lg btn-light">Login</Link>
-              </div>
+
+    //  to determine to show logged in content or non secure content
+
+    const { isAuthenticated, user } = this.props.auth;
+
+    const authenticatedContent = (
+      <div>
+        Hello Authenticated user
+      </div>
+    );
+
+    const guestContent = (
+      <div className="landing">
+      <div className="dark-overlay landing-inner text-light">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 text-center">
+              <h1 className="display-3 mb-4">Genie Shopper
+              </h1>
+              <p className="lead"> Shop electricity programs and providers with your customized usage profile.</p>
+              <hr />
+              <Link to="/register" className="btn btn-lg btn-info mr-2">Sign Up</Link>
+              <Link to="/login" className="btn btn-lg btn-light">Login</Link>
             </div>
           </div>
         </div>
+      </div>
+    </div>
+    )
+    return (
+      <div>
+      {isAuthenticated ? authenticatedContent : guestContent}
       </div>
     )
   }
