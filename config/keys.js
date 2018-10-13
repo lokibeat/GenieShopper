cb1551434843a925d15b90775189558184a7cfa3
lokibeat <37983312+lokibeat@users.noreply.github.com>
2018-10-13 11:42:21 -0500

    mongo environment update

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-    mongoURI:
-    'mongodb://genietest:genietest1@ds115753.mlab.com:15753/genieshopper',
+    mongoURI:process.env.MONGODB_URI || 'mongodb://genietest:genietest1@ds115753.mlab.com:15753/genieshopper',
     secretOrKey: 'secret'
 };
