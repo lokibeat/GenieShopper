a4709ba5b867bda6a5a6f29fd6c631254096eb06
lokibeat <37983312+lokibeat@users.noreply.github.com>
2018-10-13 12:21:50 -0500

    after yarn build

---
+++ client/build/precache-manifest.27adf3675ed2402a4059d6edc3135516.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "d45e3acd04be1edb94b1e6c11a10eddb",
+    "url": "/static/media/five_bulbs.d45e3acd.jpg"
+  },
+  {
+    "revision": "cdb4d5af4257351453a5",
+    "url": "/static/css/main.fd994ef2.chunk.css"
+  },
+  {
+    "revision": "1076a425439cec037b59",
+    "url": "/static/js/1.1076a425.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "611d4d6e9ec7e2031196112d8d2880a1",
+    "url": "/static/media/appartment.611d4d6e.jpeg"
+  },
+  {
+    "revision": "370e5cacce3355a82f4df46337b8f132",
+    "url": "/static/media/house.370e5cac.jpeg"
+  },
+  {
+    "revision": "a1477dd2647c989b4838a8ba43f0c7bf",
+    "url": "/static/media/mixed.a1477dd2.jpeg"
+  },
+  {
+    "revision": "cdb4d5af4257351453a5",
+    "url": "/static/js/main.cdb4d5af.chunk.js"
+  },
+  {
+    "revision": "774b07247cda6d4d7fa2c986e402b609",
+    "url": "/static/media/cable_clouds.774b0724.jpg"
+  },
+  {
+    "revision": "1c6c2aa8053434fcdf8fe8b3f87972b5",
+    "url": "/static/media/sun_enrgy.1c6c2aa8.jpg"
+  },
+  {
+    "revision": "5560fe62d02c5b02d132f9b8896caa14",
+    "url": "/static/media/blur-bright-bulb-813332.5560fe62.jpg"
+  },
+  {
+    "revision": "6c0ea6c7f80613772b501b2933fea2b4",
+    "url": "/static/media/evening.6c0ea6c7.jpg"
+  },
+  {
+    "revision": "f1efd89f0e06449a20bd02e76a9464d7",
+    "url": "/static/media/three_bulbs.f1efd89f.jpg"
+  },
+  {
+    "revision": "4c3888ccb25d12ad006f0f9488fa701d",
+    "url": "/index.html"
+  }
+];
